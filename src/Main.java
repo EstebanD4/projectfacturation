2b32896cac165ebb7e1fe2c85ff8cabb032fd67f
essteban <esteban.dufau@ynov.com>
2026-01-06 22:53:22 +0100

    Refactor Main class to include a main method and display client information

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,11 @@
-public class Main
-{
-    Clients client = new Clients("Jane", "Doe", "qjzhdqjhgjhsgd","0645555555");
-    client.getFirstName();
-    System.out.
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the client page of our application!");
+        Clients client1 = new Clients("John", "Doe", "prout@gmail.com", "123-456-7890");
+        System.out.println("Client Information:");
+        System.out.println("First Name: " + client1.getFirstName());
+        System.out.println("Last Name: " + client1.getLastName());
+        System.out.println("Email: " + client1.getEmail());
+        System.out.println("Phone Number: " + client1.getPhoneNumber());
+    }
 }
